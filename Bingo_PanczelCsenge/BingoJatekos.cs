09261abe985bd672ac7942913c4a17d1f17169ad
Panczel24 <pcsenge8@gmail.com>
2026-03-04 09:21:41 +0100

    bingojatekos class metódusokkal kész (talán)

--- Bingo_PanczelCsenge/BingoJatekos.cs
+++ Bingo_PanczelCsenge/BingoJatekos.cs
@@ -39,6 +39,101 @@ namespace Bingo_PanczelCsenge
             }
         }
 
-       
+        public void SorsoltSzamotJelol(int szam)
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    if (Kartya[i, j] == szam)
+                        Jelolve[i, j] = true;
+        }
+
+        public bool BingoEll()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                bool bingo = true;
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!Jelolve[i, j])
+                    {
+                        bingo = false;
+                        break;
+                    }
+                }
+
+                if (bingo)
+                    return true;
+            }
+
+            for (int j = 0; j < 5; j++)
+            {
+                bool bingo = true;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (!Jelolve[i, j])
+                    {
+                        bingo = false;
+                        break;
+                    }
+                }
+
+                if (bingo)
+                    return true;
+            }
+
+            bool atlo1 = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (!Jelolve[i, i])
+                {
+                    atlo1 = false;
+                    break;
+                }
+            }
+
+            if (atlo1)
+                return true;
+
+            bool atlo2 = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (!Jelolve[i, 4 - i])
+                {
+                    atlo2 = false;
+                    break;
+                }
+            }
+
+            if (atlo2)
+                return true;
+
+            return false;
+        }
+
+        public void KartyaKiir()
+        {
+            Console.WriteLine(Nev);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (Jelolve[i, j])
+                    {
+                        if (Kartya[i, j] == null)
+                            Console.Write(" X".PadLeft(4));
+                        else
+                            Console.Write(Kartya[i, j].ToString().PadLeft(4));
+                    }
+                    else
+                    {
+                        Console.Write(" 0".PadLeft(4));
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
     }
 }
