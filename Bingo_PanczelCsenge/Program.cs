0c0fe17ca242b65f9fd99f839caa815a79e4e1b2
Panczel24 <pcsenge8@gmail.com>
2026-03-04 10:02:06 +0100

    program.cs befejezese

--- Bingo_PanczelCsenge/Program.cs
+++ Bingo_PanczelCsenge/Program.cs
@@ -33,7 +33,46 @@
             bool vanBingo = false;
             int sorszam = 1;
 
-           
+            while (!vanBingo)
+            {
+                int szam;
+
+                do
+                {
+                    szam = rnd.Next(1, 76);
+                }
+                while (huzott.Contains(szam));
+
+                huzott.Add(szam);
+
+                Console.WriteLine($"{sorszam,3}. húzás: {szam}");
+                sorszam++;
+
+                foreach (var j in jatekosok)
+                    j.SorsoltSzamotJelol(szam);
+
+                vanBingo = false;
+                for (int i = 0; i < jatekosok.Count; i++)
+                {
+                    if (jatekosok[i].BingoEll())
+                    {
+                        vanBingo = true;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("8. feladat");
+
+            foreach (var j in jatekosok)
+            {
+                if (j.BingoEll())
+                {
+                    j.KartyaKiir();
+                }
+            }
         }
     }
 }
