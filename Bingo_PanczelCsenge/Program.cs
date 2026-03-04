79ec342bf492bae9cdd4cb5d0f8f80c8c5d831a8
Panczel24 <pcsenge8@gmail.com>
2026-03-04 08:16:57 +0100

    Add project files.

---
+++ Bingo_PanczelCsenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bingo_PanczelCsenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
